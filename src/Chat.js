4ed7bb47a5c6a153c0c9f42fbdb26744e4b2f07d
sumit <sumitmishrab@gmail.com>
2021-06-05 02:50:34 +0530

    fourth commit

---
+++ src/Chat.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Chat.css'
+import {Avatar} from '@material-ui/core'
+
+function Chat() {
+    return (
+        <div className="chat">
+
+            <div className="chat__header">
+                <Avatar/>
+            
+            </div>
+            <div className="chat__body">
+
+            </div>
+            <div className="chat__footer">
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Chat
