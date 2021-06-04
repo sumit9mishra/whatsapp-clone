0a9b083d6e2fde8433e1aff2aad352b109a2f8d8
sumit <sumitmishrab@gmail.com>
2021-06-05 00:54:07 +0530

    "second commit"

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Sidebar from './Sidebar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__body">
+        <Sidebar />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar, IconButton }  from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons'
+// import SidebarChat from './SidebarChat'
+
+
+
+function Sidebar() {
+    return (
+        <div className = "sidebar">
+           <div className="sidebar__header">
+               <Avatar/>
+               <div className="sidebar__headerRight">
+                    <IconButton><DonutLargeIcon /> </IconButton>
+                   
+                   
+                    <IconButton><ChatIcon /></IconButton>
+                   
+                    <IconButton> <MoreVertIcon /></IconButton>
+                  
+               </div>
+
+           </div>
+
+           <div className="sidebar__search">
+               <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="search or start new chat" type="text" />
+               </div>
+           </div>
+           <div className="sidebar__chats">
+               <h1>sidebar chat</h1>
+
+               {/* <SidebarChat/> */}
+
+           </div>
+
+        </div>
+    )
+}
+
+export default Sidebar
